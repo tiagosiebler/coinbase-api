848fa11f44122c5df86bafc62465a76e37307f09
Jerko J <83344666+JJ-Cro@users.noreply.github.com>
2024-09-12 13:45:20 +0200

    feat(): added Full CoinbaseExchange  Rest client

---
+++ src/types/response/coinbase-exchange.ts
@@ -0,0 +1,95 @@
+/**
+ *
+ * Accounts Endpoints
+ *
+ */
+
+/**
+ *
+ * Address book Endpoints
+ *
+ */
+
+/**
+ *
+ * Coinbase Account Endpoints
+ *
+ */
+
+/**
+ *
+ * Conversions Endpoints
+ *
+ */
+
+/**
+ *
+ * Currencies Endpoints
+ *
+ */
+
+/**
+ *
+ * Transfers Endpoints
+ *
+ */
+
+/**
+ *
+ * Fees Endpoints
+ *
+ */
+
+/**
+ *
+ * Orders Endpoints
+ *
+ */
+
+/**
+ *
+ * Loans Endpoints
+ *
+ */
+
+/**
+ *
+ * Coinbase Price Oracle Endpoints
+ *
+ */
+
+/**
+ *
+ * Products Endpoints
+ *
+ */
+
+/**
+ *
+ * Profiles Endpoints
+ *
+ */
+
+/**
+ *
+ * Reports Endpoints
+ *
+ */
+
+/**
+ *
+ * Travel Rule Endpoints
+ *
+ */
+
+/**
+ *
+ * Users Endpoints
+ *
+ */
+
+/**
+ *
+ * Wrapped Assets Endpoints
+ *
+ */
