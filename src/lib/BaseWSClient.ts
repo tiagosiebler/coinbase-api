90cce6641733e1771b249adb332cd2b4dae81858
JJ-Cro <bryanadams3312@gmail.com>
2025-02-25 09:41:14 +0100

    chore(): updated safeTerminate calls

--- src/lib/BaseWSClient.ts
+++ src/lib/BaseWSClient.ts
@@ -341,7 +341,7 @@ export abstract class BaseWebsocketClient<
     const ws = this.getWs(wsKey);
     ws?.close();
     if (force) {
-      safeTerminateWs(ws, true);
+      safeTerminateWs(ws);
     }
   }
 
@@ -531,7 +531,7 @@ export abstract class BaseWebsocketClient<
 
       this.clearPongTimer(wsKey);
 
-      safeTerminateWs(this.getWs(wsKey));
+      safeTerminateWs(this.getWs(wsKey), true);
     }, this.options.pongTimeout);
   }
 
